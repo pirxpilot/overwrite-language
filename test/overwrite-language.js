a50a5b9f3b1397930e63766841952b24478c8292
Damian Krzeminski <pirxpilot@code42day.com>
2016-06-20 17:35:35 +0200

    add language detection from TLD
    use top level domain to determine language
    example.fr - would overwrite language to French, example.de to German
    etc.

--- test/overwrite-language.js
+++ test/overwrite-language.js
@@ -1,19 +1,74 @@
 var overwriteLanguage = require('../');
 
 describe('overwrite-language node module', function () {
-  it('does not change req.lang if present', function (done) {
+
+  beforeEach(function() {
     var locale = {
       supportedLanguages: ['de', 'fr', 'pl', 'en-GB', 'en-US'],
       defaultLanguage: 'en'
     };
-    var ol = overwriteLanguage(locale);
+    this.ol = overwriteLanguage(locale);
+    this.res = {
+      cookie: function(name, value) {
+        this._cookie[name] = value;
+      },
+      clearCookie: function() {
+        delete this._cookie;
+      },
+      _cookie: {},
+    };
+  });
+
+  it('does not change req.lang if present', function (done) {
     var req = {
+      hostname: 'www.example.fr',
       lang: 'be'
     };
 
-    ol(req, {}, function(err) {
+    this.ol(req, {}, function(err) {
       req.should.have.property('lang', 'be');
       done(err);
     });
   });
+
+  it('detects language from hostname', function (done) {
+    var req = {
+      hostname: 'www.example.fr'
+    };
+
+    this.ol(req, {}, function(err) {
+      req.should.have.property('lang', 'fr');
+      done(err);
+    });
+  });
+
+  it('detects language from query parameter', function (done) {
+    var req = {
+      hostname: 'www.example.com',
+      query: { hl: 'pl' }
+    };
+    var cookie = this.res._cookie;
+
+    this.ol(req, this.res, function(err) {
+      req.should.have.property('lang', 'pl');
+      cookie.should.have.property('hl', 'pl');
+      done(err);
+    });
+  });
+
+  it('detects language from cookie', function (done) {
+    var req = {
+      hostname: 'www.example.com',
+      query: {},
+      cookies: {
+        hl: 'de'
+      }
+    };
+
+    this.ol(req, this.res, function(err) {
+      req.should.have.property('lang', 'de');
+      done(err);
+    });
+  });
+
 });
